3915c466d8cc866c08ba06fb59cd4fcb53326faf
Kelemen Péter <peter.kelemen456@gmail.com>
2023-05-06 16:25:23 +0200

    Jumping sortof working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject tile;
-    int nrOfTiles = 0;
-
+    //int nrOfTiles = 0;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+
+        /*
         GameObject tiles = Instantiate(tile, new Vector3(0,0,0),
                 Quaternion.identity); // Spawning one in the center
         spawnTiles();
+        */
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     void spawnTiles()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    //bool isGrounded = true;
+    public GameManager gameManager;
+    Rigidbody rb;
+    public GameObject tile;
+    float speed = 5;
+    public float upForce = 13;
+    public float physicsMultiplier = 1;
+    float auxPM;
+    public float speedMultiplier;
+    bool jumped = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Physics.gravity *= physicsMultiplier;
+        rb = GetComponent<Rigidbody>();
     }
 
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered jump");
+        // rb.AddForce(Vector3.up * upForce/2, ForceMode.Impulse);
 
+
+    }
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.AddForce(Vector3.up * upForce, ForceMode.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.D) && transform.position.x < 5)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A) && transform.position.x > -5)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
 }
